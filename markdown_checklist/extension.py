cbf0266f8bb65fe4e80c580588657560ccb144f5
kyle roux <kyle@level2designs.com>
2015-02-11 11:30:21 -0800

    removed disable and wrapped in label

--- markdown_checklist/extension.py
+++ markdown_checklist/extension.py
@@ -28,11 +28,11 @@ class ChecklistPostprocessor(Postprocessor):
 
     def run(self, html):
         html = re.sub(self.pattern, self._convert_checkbox, html)
-        before = '<ul>\n<li><input type="checkbox"'
+        before = '<ul>\n<li><label><input type="checkbox"'
         after = before.replace('<ul>', '<ul class="checklist">')
         return html.replace(before, after)
 
     def _convert_checkbox(self, match):
         state = match.group(1)
         checked = ' checked' if state != ' ' else ''
-        return '<li><input type="checkbox" disabled%s>' % checked
+        return '<li><input type="checkbox" %s>' % checked
